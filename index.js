72411e220fd96b3169055ab562e444ecce46dd0b
rohil1418 <rohilk592@gmail.com>
2026-09-09 20:06:37 +0530

    Fix add command syntax with proper space and positional argument

--- index.js
+++ index.js
@@ -10,25 +10,31 @@ const { revertRepo } = require("./controllers/revert.js");
 
 yargs(hideBin(process.argv))
     .command("init", "Initialised a new repository", {}, initRepo)
-    .command("add<file>",
+    .command("add <file>",
         "Add a file to the repository",
         (yargs) => {
             yargs.positional("file", {
                 describe: " File to add to the staging area",
                 type: "string",
             });
-        }, addRepo)
-        .command("commit<message>",
+        },
+        (argv) => {
+            addRepo(argv.file);
+        }
+    )
+    .command("commit <message>",
         "commit the staged files",
         (yargs) => {
             yargs.positional("message", {
                 describe: " commit message",
                 type: "string",
             });
-        }, commitRepo)
-        .command("pull", "pull commits from S3", {}, pullRepo)
-        .command("push", "push commits from S3", {}, pushRepo)
-        .command("revert<commitID>",
+        }, (argv) => {
+            commitRepo(argv.message);
+        })
+    .command("pull", "pull commits from S3", {}, pullRepo)
+    .command("push", "push commits from S3", {}, pushRepo)
+    .command("revert<commitID>",
         "Revert to a specific commit",
         (yargs) => {
             yargs.positional("commitID", {
