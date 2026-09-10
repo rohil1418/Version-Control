a85b3e798d3026e3b9950c13d075cd6f1c392523
rohil1418 <rohilk592@gmail.com>
2026-09-10 19:59:33 +0530

    Update push command to use local filesystem instead of S3

--- controllers/push.js
+++ controllers/push.js
@@ -1,5 +1,33 @@
-async function pushRepo(params) {
-    console.log("Push commad called");
+const fs = require("fs").promises;
+const path = require("path");
+
+const REMOTE_PATH = path.resolve(process.cwd(), "remote-storage");
+
+async function pushRepo() {
+    const repoPath = path.resolve(process.cwd(), ".apnaGit");
+    const commitsPath = path.join(repoPath, "commits");
+
+    try {
+        const commitDirs = await fs.readdir(commitsPath);
+
+        for (const commitDir of commitDirs) {
+            const commitPath = path.join(commitsPath, commitDir);
+            const files = await fs.readdir(commitPath);
+
+            const remoteCommitPath = path.join(REMOTE_PATH, "commits", commitDir);
+            await fs.mkdir(remoteCommitPath, { recursive: true });
+
+            for (const file of files) {
+                const sourceFile = path.join(commitPath, file);
+                const destFile = path.join(remoteCommitPath, file);
+                await fs.copyFile(sourceFile, destFile);
+            }
+        }
+
+        console.log("All commits pushed to remote storage");
+    } catch (err) {
+        console.error("Error pushing to remote: ", err);
+    }
 }
 
-module.exports = {pushRepo};
+module.exports = { pushRepo };
