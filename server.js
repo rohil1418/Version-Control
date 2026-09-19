2ea50638e4ea0674332efb783bc8b14327abef1e
rohil1418 <rohilk592@gmail.com>
2026-09-19 09:29:46 +0530

    Enable CORS for frontend requests

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -8,6 +9,7 @@ const issueRoutes = require("./routes/issueRoutes.js");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/repos", repoRoutes);
